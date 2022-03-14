1e1ef5d587142689137a8f429c92e5f306970c2f
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-03-15 00:38:46 +0800

    wip - synchronizing a recursive descent parser

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -129,4 +129,25 @@ class Parser {
         Lox.error(token, message);
         return new ParseError();
     }
+
+    private void synchronize() {
+        advance();
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON)
+                return;
+            switch (peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                    return;
+                default:
+            }
+            advance();
+        }
+    }
 }
